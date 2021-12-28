7eff8371b110658838cf4bc67e4884a458164c0c
Nurul-GC <nuruldecarvalho@gmail.com>
2021-12-28 06:12:47 +0200

    Update tests/*

--- tests/enc_test.py
+++ tests/enc_test.py
@@ -1,8 +1,7 @@
-from gcrypter.encrypt import encrypt
-from gcrypter.decrypt import decrypt
+from gcrypter import encrypt, decrypt
 
 if __name__ == '__main__':
-    word_enc = encrypt("/home/nurul-gc/Projects/PycharmProjects/pypa-gcrypter/venv/bin/python /home/nurul-gc/Projects/PycharmProjects/pypa-gcrypter/src/tests/enc_test.py")
+    word_enc = encrypt("love")
     print(word_enc)
     word_dec = decrypt(word_enc)
     print(word_dec)
