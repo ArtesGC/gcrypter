13a5e4a579be01046f78d472abee50f90134628f
Nurul-GC <nuruldecarvalho@gmail.com>
2021-12-28 05:22:33 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,28 +1,35 @@
-from distutils.core import setup
+from setuptools import setup
+
+with open('README.md', 'r') as rf:
+    longdesc = rf.read()
 
 setup(
         name='gcrypter',
         packages=['gcrypter'],
-        version='0.1',  # Start with a small number and increase it with every change you make
-        license='BSD 3-Clause',  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-        description='Is an encryption algorithm based on bytes and their correspondent numbers to encode strings',  # Give a short description about your library
-        author='Nurul-GC',  # Type in your name
-        author_email='nuruldecarvalhol@gmail.com',  # Type in your E-Mail
-        url='https://github.com/Nurul-GC/gcrypter',  # Provide either the link to your github or to your website
-        download_url='https://github.com/Nurul-GC/gcrypter/archive/refs/tags/v0.1.zip',  # I explain this later on
-        keywords=['python-encryption', 'encryption-decryption', 'encryption-algorithm'],  # Keywords that define your package best
+        version='0.1',
+        license='BSD 3-Clause',
+        description='Encryption algorithm based on bytes and their correspondent numbers to encode strings',
+        long_description=longdesc,
+        long_description_content_type="text/markdown",
+        author='Nurul-GC',
+        author_email='nuruldecarvalhol@gmail.com',
+        url='https://github.com/Nurul-GC/gcrypter',
+        download_url='https://github.com/Nurul-GC/gcrypter/archive/refs/tags/v0.1.zip',
+        project_urls={
+                "Bug Tracker": "https://github.com/Nurul-GC/gcrypter/issues",
+        },
+        keywords=['python-encryption', 'encryption-decryption', 'encryption-algorithm'],
         install_requires=[],
         classifiers=[
-                'Development Status :: 5 - Production/Stable',  # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-                'Intended Audience :: Developers',  # Define that your audience are developers
+                'Development Status :: 5 - Production/Stable',
+                'Intended Audience :: Developers',
                 'Topic :: Software Development :: Build Tools',
-                'License :: OSI Approved :: BSD 3-Clause License',  # Again, pick a license
-                'Programming Language :: Python :: 3',  # Specify which pyhton versions that you want to support
-                'Programming Language :: Python :: 3.4',
-                'Programming Language :: Python :: 3.5',
+                'License :: OSI Approved :: BSD 3-Clause License',
+                'Programming Language :: Python :: 3',
                 'Programming Language :: Python :: 3.6',
                 'Programming Language :: Python :: 3.7',
                 'Programming Language :: Python :: 3.8',
                 'Programming Language :: Python :: 3.9',
         ],
+        python_requires=">=3.6",
 )
